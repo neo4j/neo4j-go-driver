e73880ba2f4b89ecbf3ed718299fab4223fcd5a4
Peter Wilhelmsson <peter.wilhelmsson@neo4j.com>
2020-05-25 08:14:37 +0200

    testkit: Backend for using shared integration tests
    Backend for running "generic" conformance tests from a test suite shared
    among drivers implemented in other languages. The conformance tests are
    in this repository:
      https://github.com/neo4j-drivers/testkit

---
+++ testkit-backend/2cypher.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
+)
+
+// Converts native type to proxied "cypher" to be sent to frontend.
+func nativeToCypher(v interface{}) map[string]interface{} {
+	if v == nil {
+		return map[string]interface{}{"name": "CypherNull", "data": nil}
+	}
+	switch x := v.(type) {
+	case int64:
+		return valueResponse("CypherInt", x)
+	case string:
+		return valueResponse("CypherString", x)
+	case []interface{}:
+		values := make([]interface{}, len(x))
+		for i, y := range x {
+			values[i] = nativeToCypher(y)
+		}
+		return valueResponse("CypherList", values)
+	case []string:
+		values := make([]interface{}, len(x))
+		for i, y := range x {
+			values[i] = nativeToCypher(y)
+		}
+		return valueResponse("CypherList", values)
+	case map[string]interface{}:
+		values := make(map[string]interface{})
+		for k, v := range x {
+			values[k] = nativeToCypher(v)
+		}
+		return valueResponse("CypherMap", values)
+	case neo4j.Node:
+		return map[string]interface{}{
+			"name": "Node", "data": map[string]interface{}{
+				"id":     nativeToCypher(x.Id),
+				"labels": nativeToCypher(x.Labels),
+				"props":  nativeToCypher(x.Props),
+			}}
+	}
+	panic(fmt.Sprintf("Don't know how to patch %T", v))
+}
+
+// Helper to wrap proxied "cypher" value into a response
+func valueResponse(name string, v interface{}) map[string]interface{} {
+	return map[string]interface{}{"name": name, "data": map[string]interface{}{"value": v}}
+}
