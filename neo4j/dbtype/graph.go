5cf97e1b8a82d470e4f6d0a60ea04908f851122b
Peter Wilhelmsson <peter.wilhelmsson@neo4j.com>
2020-08-07 13:28:48 +0200

    Simplified Path struct

--- neo4j/dbtype/graph.go
+++ neo4j/dbtype/graph.go
@@ -35,61 +35,11 @@ type Relationship struct {
 	Props   map[string]interface{} // Properties of this Relationship.
 }
 
-type RelNode struct {
-	Id    int64
-	Type  string
-	Props map[string]interface{}
-}
-
 // Path represents a directed sequence of relationships between two nodes.
 // This generally represents a traversal or walk through a graph and maintains a direction separate from that of any
 // relationships traversed. It is allowed to be of size 0, meaning there are no relationships in it. In this case,
 // it contains only a single node which is both the start and the end of the path.
 type Path struct {
-	Nodes    []*Node // All the nodes in the path.
-	RelNodes []*RelNode
-	Indexes  []int
-}
-
-func (p *Path) Relationships() []*Relationship {
-	num := len(p.Indexes) / 2
-	if num == 0 {
-		return nil
-	}
-	rels := make([]*Relationship, 0, num)
-
-	i := 0
-	n1 := p.Nodes[0]
-	for num > 0 {
-		relni := p.Indexes[i]
-		i++
-		n2i := p.Indexes[i]
-		i++
-		num--
-		var reln *RelNode
-		var n1start bool
-		if relni < 0 {
-			reln = p.RelNodes[(relni*-1)-1]
-		} else {
-			reln = p.RelNodes[relni-1]
-			n1start = true
-		}
-		n2 := p.Nodes[n2i]
-
-		rel := &Relationship{
-			Id:    reln.Id,
-			Type:  reln.Type,
-			Props: reln.Props,
-		}
-		if n1start {
-			rel.StartId = n1.Id
-			rel.EndId = n2.Id
-		} else {
-			rel.StartId = n2.Id
-			rel.EndId = n1.Id
-		}
-		rels = append(rels, rel)
-		n1 = n2
-	}
-	return rels
+	Nodes         []*Node // All the nodes in the path.
+	Relationships []*Relationship
 }
