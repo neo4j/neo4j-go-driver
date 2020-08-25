e44d11aac8c10a14d29424d4a53fac831ccbaf76
Peter Wilhelmsson <peter.wilhelmsson@neo4j.com>
2020-08-25 15:15:06 +0200

    testkit: Handle error when writing response Backend write response error checks

--- testkit-backend/backend.go
+++ testkit-backend/backend.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 
 	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 )
@@ -13,7 +14,7 @@ import (
 // Tracks all objects (and errors) that is created by testkit frontend.
 type backend struct {
 	rd             *bufio.Reader // Socket to read requests from
-	wr             *bufio.Writer // Socket to write responses (and logs) on
+	wr             io.Writer     // Socket to write responses (and logs) on, don't buffer (WriteString on bufio was weird...)
 	drivers        map[string]neo4j.Driver
 	sessionStates  map[string]*sessionState
 	results        map[string]neo4j.Result
@@ -36,7 +37,7 @@ const (
 	retryable_negative = -1
 )
 
-func newBackend(rd *bufio.Reader, wr *bufio.Writer) *backend {
+func newBackend(rd *bufio.Reader, wr io.Writer) *backend {
 	return &backend{
 		rd:             rd,
 		wr:             wr,
@@ -57,6 +58,12 @@ func (e *clientError) Error() string {
 	return e.msg
 }
 
+func (b *backend) writeLine(s string) error {
+	bs := []byte(s + "\n")
+	_, err := b.wr.Write(bs)
+	return err
+}
+
 // Reads and writes to the socket until it is closed
 func (b *backend) serve() {
 	for b.process() {
@@ -73,15 +80,17 @@ func (b *backend) writeError(err error) {
 	// Convert error if it is a known type of error.
 	// This is very simple right now, no extra information is sent at all just keep
 	// track of this error so that we can reuse the real thing within a retryable tx
-	if neo4j.IsTransientError(err) {
+	if neo4j.IsTransientError(err) || neo4j.IsClientError(err) || neo4j.IsServiceUnavailable(err) || neo4j.IsAuthenticationError(err) || neo4j.IsSecurityError(err) {
 		id := b.setError(err)
 		b.writeResponse("DriverError", map[string]interface{}{"id": id})
 		return
 	}
 
+	// This is an error that originated in frontend
 	clientErr, isClientErr := err.(*clientError)
 	if isClientErr {
 		b.writeResponse("ClientError", map[string]interface{}{"msg": clientErr.msg})
+		return
 	}
 
 	// TODO: Return the other kinds of errors as well...
@@ -105,8 +114,7 @@ func (b *backend) process() bool {
 	for {
 		line, err := b.rd.ReadString('\n')
 		if err != nil {
-			b.wr.WriteString(err.Error() + "\n")
-			b.wr.Flush()
+			b.writeLine(err.Error())
 			return false
 		}
 
@@ -128,6 +136,7 @@ func (b *backend) process() bool {
 			if !in_request {
 				panic("Line while not in request")
 			}
+
 			request = request + line
 		}
 	}
@@ -140,8 +149,18 @@ func (b *backend) writeResponse(name string, data interface{}) {
 	if err != nil {
 		panic(err.Error())
 	}
-	b.wr.WriteString("#response begin\n" + string(responseJson) + "\n" + "#response end\n")
-	b.wr.Flush()
+	err = b.writeLine("#response begin")
+	if err != nil {
+		panic(err.Error())
+	}
+	err = b.writeLine(string(responseJson))
+	if err != nil {
+		panic(err.Error())
+	}
+	err = b.writeLine("#response end")
+	if err != nil {
+		panic(err.Error())
+	}
 }
 
 func (b *backend) toRequest(s string) map[string]interface{} {
@@ -171,7 +190,7 @@ func (b *backend) handleRequest(req map[string]interface{}) {
 			return
 		}
 		driver, err := neo4j.NewDriver(data["uri"].(string), authToken, func(c *neo4j.Config) {
-			c.Log = &streamLog{wr: b.wr} //neo4j.ConsoleLogger(neo4j.DEBUG)
+			c.Log = &streamLog{writeLine: b.writeLine}
 		})
 		if err != nil {
 			b.writeError(err)
