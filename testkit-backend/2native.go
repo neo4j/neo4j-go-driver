7ee3b7c75b6ed80d0b9cb1936b95e613fb3c00be
Peter Wilhelmsson <peter.wilhelmsson@neo4j.com>
2020-09-03 10:12:03 +0200

    testkit: Conversion to native fix

--- testkit-backend/2native.go
+++ testkit-backend/2native.go
@@ -7,9 +7,12 @@ import (
 // Converts received proxied "cypher" types to Go native types.
 func cypherToNative(c interface{}) interface{} {
 	m := c.(map[string]interface{})
+	d := m["data"].(map[string]interface{})
 	switch m["name"] {
 	case "CypherString":
-		return m["data"].(map[string]interface{})["value"].(string)
+		return d["value"].(string)
+	case "CypherInt":
+		return int64(d["value"].(float64))
 	}
 	panic(fmt.Sprintf("Don't know how to convert cypher %s to native", m))
 }
