8f4be851f89051ffffc33ff1bdd0108b1cdce7a8
Florent Biville <florent.biville@neo4j.com>
2022-05-05 16:07:38 +0200

    Set HTTPS links everywhere

--- neo4j/internal/bolt/bolt3.go
+++ neo4j/internal/bolt/bolt3.go
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) "Neo4j"
- * Neo4j Sweden AB [http://neo4j.com]
+ * Neo4j Sweden AB [https://neo4j.com]
  *
  * This file is part of Neo4j.
  *
@@ -8,7 +8,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
@@ -212,7 +212,7 @@ func (b *bolt3) Connect(ctx context.Context, minor int, auth map[string]interfac
 }
 
 func (b *bolt3) TxBegin(ctx context.Context, txConfig idb.TxConfig) (idb.
-	TxHandle, error) {
+TxHandle, error) {
 	// Ok, to begin transaction while streaming auto-commit, just empty the stream and continue.
 	if b.state == bolt3_streaming {
 		if err := b.bufferStream(ctx); err != nil {
--- test-stress/testcontext.go
+++ test-stress/testcontext.go
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) "Neo4j"
- * Neo4j Sweden AB [http://neo4j.com]
+ * Neo4j Sweden AB [https://neo4j.com]
  *
  * This file is part of Neo4j.
  *
@@ -8,7 +8,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,8 +29,8 @@ import (
 
 // TestContext provides state data shared across tests
 type TestContext struct {
-	driver   neo4j.Driver
-	stop     int32
+	driver    neo4j.Driver
+	stop      int32
 	bookmarks atomic.Value
 
 	readNodeCountsByServer sync.Map
