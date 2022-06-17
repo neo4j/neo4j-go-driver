5ad8842011aecce27e9c0fbc43119a8e3f4e861f
Florent Biville <florent.biville@neo4j.com>
2022-06-17 17:36:34 +0200

    Implement support for UTC DateTime structures

--- neo4j/internal/bolt/hydrator.go
+++ neo4j/internal/bolt/hydrator.go
@@ -467,8 +467,12 @@ func (h *hydrator) value() interface{} {
 			return h.point3d(n)
 		case 'F':
 			return h.dateTimeOffset(n)
+		case 'I':
+			return h.utcDateTimeOffset(n)
 		case 'f':
 			return h.dateTimeNamedZone(n)
+		case 'i':
+			return h.utcDateTimeNamedZone(n)
 		case 'd':
 			return h.localDateTime(n)
 		case 'D':
@@ -710,24 +714,51 @@ func (h *hydrator) point3d(n uint32) interface{} {
 
 func (h *hydrator) dateTimeOffset(n uint32) interface{} {
 	h.unp.Next()
-	secs := h.unp.Int()
+	seconds := h.unp.Int()
 	h.unp.Next()
-	nans := h.unp.Int()
+	nanos := h.unp.Int()
 	h.unp.Next()
-	offs := h.unp.Int()
-	t := time.Unix(secs, nans).UTC()
-	l := time.FixedZone("Offset", int(offs))
-	return time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), l)
+	offset := h.unp.Int()
+	// time.Time in local timezone, e.g. 15th of June 2020, 15:30 in Paris (UTC+2h)
+	unixTime := time.Unix(seconds, nanos)
+	// time.Time computed in UTC timezone, e.g. 15th of June 2020, 13:30 in UTC
+	utcTime := unixTime.UTC()
+	// time.Time **copied** as-is in the target timezone, e.g. 15th of June 2020, 13:30 in target tz
+	timeZone := time.FixedZone("Offset", int(offset))
+	return time.Date(
+		utcTime.Year(),
+		utcTime.Month(),
+		utcTime.Day(),
+		utcTime.Hour(),
+		utcTime.Minute(),
+		utcTime.Second(),
+		utcTime.Nanosecond(),
+		timeZone,
+	)
+}
+
+func (h *hydrator) utcDateTimeOffset(n uint32) interface{} {
+	h.unp.Next()
+	seconds := h.unp.Int()
+	h.unp.Next()
+	nanos := h.unp.Int()
+	h.unp.Next()
+	offset := h.unp.Int()
+	timeZone := time.FixedZone("Offset", int(offset))
+	return time.Unix(seconds, nanos).In(timeZone)
 }
 
 func (h *hydrator) dateTimeNamedZone(n uint32) interface{} {
 	h.unp.Next()
-	secs := h.unp.Int()
+	seconds := h.unp.Int()
 	h.unp.Next()
-	nans := h.unp.Int()
+	nanos := h.unp.Int()
 	h.unp.Next()
-	zone := h.unp.String()
-	t := time.Unix(secs, nans).UTC()
+	zone := h.unp.String() // time.Time in local timezone, e.g. 15th of June 2020, 15:30 in Paris (UTC+2h)
+	unixTime := time.Unix(seconds, nanos)
+	// time.Time computed in UTC timezone, e.g. 15th of June 2020, 13:30 in UTC
+	utcTime := unixTime.UTC()
+	// time.Time **copied** as-is in the target timezone, e.g. 15th of June 2020, 13:30 in target tz
 	l, err := time.LoadLocation(zone)
 	if err != nil {
 		h.setErr(&db.ProtocolError{
@@ -737,7 +768,26 @@ func (h *hydrator) dateTimeNamedZone(n uint32) interface{} {
 		})
 		return nil
 	}
-	return time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), l)
+	return time.Date(utcTime.Year(), utcTime.Month(), utcTime.Day(), utcTime.Hour(), utcTime.Minute(), utcTime.Second(), utcTime.Nanosecond(), l)
+}
+
+func (h *hydrator) utcDateTimeNamedZone(n uint32) interface{} {
+	h.unp.Next()
+	secs := h.unp.Int()
+	h.unp.Next()
+	nans := h.unp.Int()
+	h.unp.Next()
+	zone := h.unp.String()
+	timeZone, err := time.LoadLocation(zone)
+	if err != nil {
+		h.setErr(&db.ProtocolError{
+			MessageType: "dateTimeNamedZone",
+			Field:       "location",
+			Err:         err.Error(),
+		})
+		return nil
+	}
+	return time.Unix(secs, nans).In(timeZone)
 }
 
 func (h *hydrator) localDateTime(n uint32) interface{} {
