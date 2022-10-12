706b325719351374e89fc1dbe291403adeb90d3a
Florent Biville <florent.biville@neo4j.com>
2022-10-12 12:21:45 +0200

    Disable VCS stamping... again :(

--- testkit/build.py
+++ testkit/build.py
@@ -13,35 +13,28 @@ def run(args, env=None):
 
 
 if __name__ == "__main__":
-    # When running inside docker, pwd (this directory containing the git repo)
-    # will be mounted into the container and it's not owned by root but whatever
-    # UID the TeamCity agent process has. Hence, we need to include this
-    # directory to the ones trusted by git (CVE-2022-24765).
-    # This allows Go to use VCS stamping
-    print("Include current folder to Git safe directories", flush=True)
-
-    currentDir = subprocess.check_output(["pwd"]).decode("utf-8").strip()
-    run(["git", "config", "--global", "--add", "safe.directory", currentDir])
+    defaultEnv = os.environ.copy()
+    defaultEnv["GOFLAGS"] = "-buildvcs=false"
 
     print("Building for current target", flush=True)
-    run(["go", "build", "-v", "./..."])
+    run(["go", "build", "-v", "./..."], env=defaultEnv)
 
     # Compile for 32 bits ARM to make sure it builds
     print("Building for 32 bits", flush=True)
-    arm32Env = os.environ.copy()
+    arm32Env = defaultEnv.copy()
     arm32Env["GOOS"] = "linux"
     arm32Env["GOARCH"] = "arm"
     arm32Env["GOARM"] = "7"
     run(["go", "build", "./..."], env=arm32Env)
 
     print("Vet sources", flush=True)
-    run(["go", "vet", "./..."])
+    run(["go", "vet", "./..."], env=defaultEnv)
 
     print("Install staticcheck", flush=True)
-    run(["go", "install", "honnef.co/go/tools/cmd/staticcheck@latest"])
+    run(["go", "install", "honnef.co/go/tools/cmd/staticcheck@latest"], env=defaultEnv)
 
     print("Run staticcheck", flush=True)
     gopath = Path(
         subprocess.check_output(["go", "env", "GOPATH"]).decode("utf-8").strip()
     )
-    run([str(gopath / "bin" / "staticcheck"), "./..."])
+    run([str(gopath / "bin" / "staticcheck"), "./..."], env=defaultEnv)
