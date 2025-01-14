a2296d70cf585a694bd8aea0c9fdabb8b1bf1cc8
Robsdedude <rouven.bauer@neo4j.com>
2025-01-14 15:56:19 +0100

    TestKit backend: use DNS hook feature flag

--- testkit-backend/backend.go
+++ testkit-backend/backend.go
@@ -1363,6 +1363,7 @@ func (b *backend) handleRequest(req map[string]any) {
 				"ConfHint:connection.recv_timeout_seconds",
 
 				// === BACKEND FEATURES FOR TESTING ===
+				"Backend:DNSResolver",
 				"Backend:MockTime",
 				"Backend:RTFetch",
 				"Backend:RTForceUpdate",
@@ -1724,10 +1725,6 @@ func testSkips() map[string]string {
 		"stub.routing.test_routing_v3.RoutingV3.test_should_fail_when_writing_on_unexpectedly_interrupting_writer_on_pull_using_tx_run":            "Won't fix - only Bolt 3 affected (not officially supported by this driver): broken servers are not removed from routing table",
 		"stub.routing.test_routing_v3.RoutingV3.test_should_fail_when_writing_on_unexpectedly_interrupting_writer_on_run_using_tx_run":             "Won't fix - only Bolt 3 affected (not officially supported by this driver): broken servers are not removed from routing table",
 		"stub.routing.test_routing_v3.RoutingV3.test_should_fail_when_writing_on_unexpectedly_interrupting_writer_using_tx_run":                    "Won't fix - only Bolt 3 affected (not officially supported by this driver): broken servers are not removed from routing table",
-
-		// Missing message support in testkit backend
-		"stub.routing.*.*.test_should_request_rt_from_all_initial_routers_until_successful_on_unknown_failure":       "Add DNS resolver TestKit message and connection timeout support",
-		"stub.routing.*.*.test_should_request_rt_from_all_initial_routers_until_successful_on_authorization_expired": "Add DNS resolver TestKit message and connection timeout support",
 
 		// To fix/to decide whether to fix
 		"stub.routing.test_routing_v*.RoutingV*.test_should_revert_to_initial_router_if_known_router_throws_protocol_errors": "Driver always uses configured URL first and custom resolver only if that fails",
