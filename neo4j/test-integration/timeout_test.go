ad2c489f6f35fcd441f50621080e83c1dcdb1346
Stephen Cathcart <stephen.cathcart@neo4j.com>
2025-08-13 13:47:30 +0100

    Update neo4j/test-integration/timeout_test.go
    Co-authored-by: Robsdedude <dev@rouvenbauer.de>

--- neo4j/test-integration/timeout_test.go
+++ neo4j/test-integration/timeout_test.go
@@ -23,7 +23,6 @@ import (
 	"time"
 
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j/config"
-
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j/test-integration/dbserver"
 )
