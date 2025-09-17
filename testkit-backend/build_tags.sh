8a06fa71306bf169cefb9c8b6967c2d263036f7f
Robsdedude <rouven.bauer@neo4j.com>
2025-09-17 18:05:57 +0200

    fixup! Merge branch '5.0' into testkit/time-warp-backend

--- testkit-backend/build_tags.sh
+++ testkit-backend/build_tags.sh
@@ -21,7 +21,7 @@ elif [ "$(echo "$version" | cut -d "." -f 1)" != "5" ]; then
 	exit 1
 fi
 
-if verlt "$version" "5.28.1"; then
+if verlt "$version" "5.28.4"; then
 	tags="internal_testkit,internal_time_mock"
 else
 	tags="internal_neo4j_go_driver_testkit,internal_neo4j_go_driver_time_mock"
